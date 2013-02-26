321af609c9d6772aa7c1241de5dd4bf00eca711e
Julien Quint <julien@igel.co.jp>
2013-02-26 11:01:24 +0900

    Renamed v to V

--- test/v.js
+++ test/v.js
@@ -1,27 +1,26 @@
 "use strict";
 
 var assert = window.chai.assert;
-var v = window.v;
 
 describe("V", function () {
   it("is defined", function () {
-    assert.isObject(v, "v is an object");
+    assert.isObject(V, "V is an object");
   });
-  it("saves any previous version of this.v in v.old_v", function () {
-    assert.strictEqual(v.old_v, "v", "previous value of v was saved");
+  it("saves any previous version of this.V in V.old_V", function () {
+    assert.strictEqual(V.old_V, "V", "previous value of V was saved");
   });
 });
 
 describe("Manipulating the DOM", function () {
-  describe("v.create_element(name, [attrs], ...)", function () {
+  describe("V.create_element(name, [attrs], ...)", function () {
     it("creates an element with the given name", function () {
-      var p = v.create_element("p");
+      var p = V.create_element("p");
       assert.strictEqual(p.localName, "p", "created p element");
     });
     it("use known namespace prefixes svg and html for the name of the element ", function () {
-      var rect = v.create_element("svg:rect");
+      var rect = V.create_element("svg:rect");
       assert.strictEqual(rect.localName, "rect", "created rect element");
-      assert.strictEqual(rect.namespaceURI, v.ns.svg, "created SVG element");
+      assert.strictEqual(rect.namespaceURI, V.ns.svg, "created SVG element");
     });
   });
 });
